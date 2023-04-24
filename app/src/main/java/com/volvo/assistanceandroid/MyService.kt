1da4a62629a936a6d259fe82cb0b5a2d994847de
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-04-24 23:01:12 +0900

    [Feat] 부팅시 서비스 자동 실행 기능 구현 #4

--- app/src/main/java/com/volvo/assistanceandroid/MyService.kt
+++ app/src/main/java/com/volvo/assistanceandroid/MyService.kt
@@ -1,9 +1,14 @@
 package com.volvo.assistanceandroid
 
-import android.app.Service
+import android.app.*
 import android.content.Intent
+import android.graphics.Color
 import android.graphics.PixelFormat
+import android.net.Uri
+import android.os.Build
 import android.os.IBinder
+import android.provider.Settings
+import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
@@ -11,23 +16,35 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.NotificationCompat
 
+const val NOTI_ID = 1000
 
 class MyService : Service() {
     private lateinit var wm: WindowManager
     private lateinit var mView: View
+
     override fun onBind(intent: Intent): IBinder? {
         return null
     }
 
     override fun onCreate() {
         super.onCreate()
+        initUi()
+        createNotification()
+    }
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("VolvoTest", "서비스가 시작되었습니다.")
+        return START_STICKY // 서비스가 강제 종료되면 재시작하도록 설정합니다.
+    }
+
+    private fun initUi(){
         val inflate = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
         wm = getSystemService(WINDOW_SERVICE) as WindowManager // wm 변수를 초기화
         val params = WindowManager.LayoutParams(
             400,
             ViewGroup.LayoutParams.WRAP_CONTENT,
-
             WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
             WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                     or WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
@@ -36,13 +53,38 @@ class MyService : Service() {
         params.gravity = Gravity.CENTER or Gravity.BOTTOM
         mView = inflate.inflate(R.layout.view_in_service, null) // mView 변수를 초기화
         mView.let {
-
             val bt = it.findViewById<View>(R.id.bt) as ImageButton
             bt.setOnClickListener {
-                bt.setImageState(intArrayOf(android.R.attr.state_pressed), true) // 버튼의 상태에 따라 이미지를 변경합니다.
+                bt.setImageResource(R.drawable.img_volvoback_logo)
             }
             wm.addView(it, params)
         }
+    }
+
+
+    private fun createNotification() {
+        val builder = NotificationCompat.Builder(this, "default")
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+        builder.setContentTitle("VoiceAssistance Service")
+        builder.setContentText("서비스 실행 중")
+        builder.color = Color.RED
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+        builder.setContentIntent(pendingIntent) // 알림 클릭 시 이동
+
+        // 알림 표시
+        val notificationManager = this.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(
+            NotificationChannel(
+                "default",
+                "기본 채널",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+        )
+        notificationManager.notify(NOTI_ID, builder.build()) // id : 정의해야하는 각 알림의 고유한 int값
+        val notification = builder.build()
+        startForeground(NOTI_ID, notification)
     }
 
     override fun onDestroy() {
