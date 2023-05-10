ec0bb10999a058cfb629dad70f5e523fe5e88d5c
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-05-10 20:19:27 +0900

    [feat] 텍스트 의도 분류를 위한 Helper 클래스 구현 #4

--- app/src/main/java/com/volvo/assistanceandroid/TextClassificationHelper.kt
+++ app/src/main/java/com/volvo/assistanceandroid/TextClassificationHelper.kt
@@ -1,17 +1,59 @@
 package com.volvo.assistanceandroid
 
 import android.content.Context
+import org.tensorflow.lite.support.label.Category
+import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.text.nlclassifier.BertNLClassifier
+import java.util.concurrent.ScheduledThreadPoolExecutor
 
 class TextClassificationHelper(
-    val context: Context,
-    val listener: TextResultsListener
+    private val context: Context,
+    private val listener: TextResultsListener
 ) {
+
+    companion object {
+        const val MODELNAME = "model.tflite"
+    }
+
+    private lateinit var bertClassifier: BertNLClassifier
+    private lateinit var executor: ScheduledThreadPoolExecutor
+
+
+    init {
+        initClassifier()
+    }
+
+    /** 텍스트 분류 모델을 초기화하는 함수  **/
+    private fun initClassifier() {
+        val baseOptionsBuilder = BaseOptions.builder()
+        val baseOptions = baseOptionsBuilder.build()
+
+        val options = BertNLClassifier.BertNLClassifierOptions
+            .builder()
+            .setBaseOptions(baseOptions)
+            .build()
+
+        bertClassifier = BertNLClassifier.createFromFileAndOptions(
+            context,
+            MODELNAME,
+            options
+        )
+    }
+
+    /** 텍스트를 분류하는 함수 **/
+    fun classify(text: String) {
+        executor = ScheduledThreadPoolExecutor(1)
+
+        executor.execute {
+            // Use the appropriate classifier based on the selected model
+            val results: List<Category> = bertClassifier.classify(text)
+            listener.onResult(results)
+        }
+    }
 
     interface TextResultsListener {
         fun onError(error: String)
-        fun onResult(results: List<Category>, inferenceTime: Long)
+        fun onResult(results: List<Category>)
     }
-
-
 
 }
